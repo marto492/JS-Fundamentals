23e2920db34562840ecd2ad45f11223ddf6b5754
Martin Ananiev <127123719+marto492@users.noreply.github.com>
2023-09-19 20:46:44 +0300

    Create bitcoinMining.js

---
+++ bitcoinMining.js
@@ -0,0 +1,10 @@
+function bitcoin(input){
+let totalGold = 0;
+let btcMines = 0;
+for (let i = 0; i < input.length; i++) {
+    let currGold = input[i];
+    if(currGold >=)
+}
+
+}
+bitcoin([100, 200, 300])
