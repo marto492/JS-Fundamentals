9b6a6ced62c048320f593b360589605421bcdd4e
Martin Ananiev <127123719+marto492@users.noreply.github.com>
2023-09-20 19:27:37 +0300

    Update bitcoinMining.js

--- bitcoinMining.js
+++ bitcoinMining.js
@@ -7,6 +7,7 @@ function bitcoin(input){
     let goldPrice = 67.51;
     let btcPrice = 11949.16;
     let totalCash = 0;
+   
 
     for(let i = 0; i <= input.length-1; i++){
         day++
@@ -20,17 +21,17 @@ function bitcoin(input){
             goldMined += input[i]
         }
 
-
-        if(goldMined * goldPrice >= btcPrice ){
-            firstDay = i;
-        }
-        
-
+        let totalCash1stDay = totalCash - btcPrice;
+      
 
         while(totalCash >= btcPrice){
             totalCash -= btcPrice
             btcmined++
+            if(totalCash == totalCash1stDay){
+                firstDay++;
+            }
         }
+      
     }
 
     if(btcmined >= 1){
@@ -38,37 +39,8 @@ function bitcoin(input){
          console.log(`Day of the first purchased bitcoin: ${firstDay}`);
     }else{
         console.log(`Bought bitcoins: ${btcmined}`);
-        console.log(`Left money: ${totalCash} lv`);
     }
 
-    console.log(`Left money: ${totalCash} lv`);
-    
-   
- 
-// let btcMines = 0;
-// let firstBought = 0;
-// let totalMoney = 0;
-// let day = 0
-// for (let i = 0; i < input.length; i++) {
-//     day++
-//     let currGold = input[i];
-//     totalMoney += input[i] * 67.51
-//     if(day % 3 == 0){
-//        currGold *= 0.70; 
-//     }
-//     if(currGold >= 176.998371){
-//         btcMines++
-//         firstBought++
-//     }
-    
-// }
-// if(btcMines > 0){
-//     console.log(`Bought bitcoins: ${btcMines}`);
-//     console.log(`Day of the first purchased bitcoin: ${firstBought}`);
-// }
-
-// totalMoney -= (btcMines * 11949.16)
-// console.log(`Left money: ${totalMoney} lv`);
-
+    console.log(`Left money: ${totalCash.toFixed(2)} lv.`);
 }
 bitcoin([3124.15, 504.212, 2511.124])
