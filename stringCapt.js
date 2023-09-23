0d6cae266bde7ff7b81ae3d087d9de2e6899ebd7
Martin Ananiev <127123719+marto492@users.noreply.github.com>
2023-09-24 02:26:45 +0300

    Create stringCapt.js

---
+++ stringCapt.js
@@ -0,0 +1,12 @@
+function stringCapt(phrase){
+let capt = '';
+
+for(let i = 0; i<=phrase.length-1; i++){
+    i++
+    let char = phrase[i];
+    char.toUpperCase 
+}
+console.log(capt);
+
+}
+stringCapt('asyougo')
