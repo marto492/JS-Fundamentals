849877c643c3a3016fe466df09e7f4f2ea6ff0f3
Martin Ananiev <127123719+marto492@users.noreply.github.com>
2023-10-07 14:28:38 +0300

    Functions-lab

---
+++ Functions/04-orders.js
@@ -0,0 +1,24 @@
+function orders(item, count) {
+    let totalprice = 0;
+  
+    switch (item) {
+      case `coffee`:
+        totalprice = count * 1.5;
+        break;
+      case `water`:
+        totalprice = count * 1.0;
+        break;
+      case `coke`:
+        totalprice = count * 1.4;
+        break;
+      case `snacks`:
+        totalprice = count * 2.0;
+        break;
+      default:
+        console.log(`Item not in stock.`);
+        break;
+    }
+    return totalprice.toFixed(2)
+  }
+  let res = orders(`coke`, 5)
+  console.log(res);
