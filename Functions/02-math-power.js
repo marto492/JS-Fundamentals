58eefcdacf93b213589dedd2c9812b989df6c13c
Martin Ananiev <127123719+marto492@users.noreply.github.com>
2023-10-07 14:22:21 +0300

    Delete Functions directory

--- Functions/02-math-power.js
+++
@@ -1,8 +0,0 @@
-function mathPower(num,pow){
-    return num**pow
-}
-
-
-
-let result = mathPower(2,8)
-console.log(result);
--- Functions/03-repeat-string.js
+++
@@ -1,9 +0,0 @@
-function repeatString(str, count){
-    let repeatedString = ``
-for(let i = 0; i < count; i++){
-   repeatedString+=str
-}
-return repeatedString
-}
-let res = repeatString(`abc`, 3)
-console.log(res);
